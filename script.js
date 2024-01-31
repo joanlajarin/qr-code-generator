5017c112a208bb5832d323421a90edceffc8daec
Joan Lajarin <joanlajarinreinares@gmail.com>
2024-01-31 15:17:12 +0100

    fix form becuase it was making the page reload

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ btnSearch.addEventListener("click",searchForQrImg )
 
 function searchForQrImg() {
     const url = document.getElementById("input-form").value
-    if(!(url === null || url === "" || !url.contains('.'))) {
+    if(!(url === null || url === "")) {
         const urlToCode = 'https://api.qrserver.com/v1/create-qr-code/?data=' + url +'&size=307x307'
 
         localStorage.setItem('urlToCode', urlToCode);
-        window.location.href = "qrcode.html"
+        window.location = "qrcode.html"
+        console.log(window.location)
+
     }
 }
